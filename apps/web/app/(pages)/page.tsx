fd030d8f656a034a3102c3b2aff9b59eea415f8b
sarinsanyal <sarinsanyal2005@gmail.com>
2026-08-09 01:29:57 +0530

    say hello to a new beginning

---
+++ apps/web/app/(pages)/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import React from "react";
+import HeroPage from "@/components/custom/homepage/Hero";
+import { Separator } from "@/components/ui/separator";
+import Features from "@/components/custom/homepage/Features";
+import Navbar from "@/components/custom/Navbar";
+import Use from "@/components/custom/homepage/HowToUse";
+import Reviews from "@/components/custom/homepage/Reviews";
+
+export default function Main() {
+  return (
+    <div className="font-(family-name:--font-geist-sans)">
+      <Navbar />
+      <HeroPage />
+      <Features />
+      <Use />
+      <Reviews />
+    </div>
+  );
+}
+
